32e263bec984a20842804b1b7e5e02bc3981b627
abinthm <abinthomas253@gmail.com>
2025-05-27 23:32:30 +0530

    new

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,16 +4,22 @@ import WhoWeAre from '../components/WhoWeAre';
 import WhatWeDo from '../components/WhatWeDo';
 import WhereWeWork from '../components/WhereWeWork';
 import ContactUs from '../components/ContactUs';
+import { motion } from 'framer-motion';
 
 const About = () => {
   return (
-    <>
+    <motion.main
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -20 }}
+      transition={{ duration: 0.4 }}
+    >
       <AboutHero />
       <WhoWeAre />
       <WhatWeDo />
       <WhereWeWork />
       <ContactUs />
-    </>
+    </motion.main>
   );
 };
 
